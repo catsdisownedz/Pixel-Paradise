bf41f0d102d3621d23c985a5d431459085d7e6ff
noah-mclain <nadam.30032415@gmail.com>
2024-05-10 20:23:22 +0300

    made scroll work using css and changed Pixel Paradise.html to index.html

--- behind_the_scenes.js
+++ behind_the_scenes.js
@@ -1,27 +1,35 @@
-{/* <script src="/path/to/flickity.pkgd.min.js"></script> */}
-const gameList = document.querySelector('.game-container');
+/* <script src="/path/to/flickity.pkgd.min.js"></script> */
+
+const gameList = document.querySelector(".game-container");
 const sounds = {};
 
 // Preload game sounds
-const soundElements = document.querySelectorAll('.game[data-sound]');
-soundElements.forEach(game => {
+const soundElements = document.querySelectorAll(".game[data-sound]");
+soundElements.forEach((game) => {
   const soundPath = game.dataset.sound;
   const audio = new Audio(soundPath);
   sounds[soundPath] = audio;
 });
 
-gameList.addEventListener('scroll', () => {
+gameList.addEventListener("scroll", () => {
   // Disable scroll snapping at the end of the list
   if (gameList.scrollWidth <= gameList.scrollLeft + gameList.clientWidth) {
-    gameList.style.scrollSnapType = 'none';
+    gameList.style.scrollSnapType = "none";
   } else {
-    gameList.style.scrollSnapType = 'x mandatory';
+    gameList.style.scrollSnapType = "x mandatory";
   }
 });
 
-gameList.addEventListener('mouseover', function(event) {
-  if (event.target.classList.contains('game')) {
+// gameList.addEventListener('mouseover', function(event) {
+//   if (event.target.classList.contains('game')) {
+//     const soundPath = event.target.dataset.sound;
+
+//   }
+// });
+
+gameList.addEventListener("mouseover", function (event) {
+  if (event.target.classList.contains("game")) {
     const soundPath = event.target.dataset.sound;
-    
+    sounds[soundPath].play();
   }
 });
