ad10e84f58c991e745a09849e0dd0c897e0c6d25
noah-mclain <nadam.30032415@gmail.com>
2024-05-10 22:51:19 +0300

    made randomness more frequent

--- Pong/pong.js
+++ Pong/pong.js
@@ -97,7 +97,7 @@ window.onload = function () {
       computer.dy = 0;
     }
     // Add some randomness to the AI
-    if (Math.random() < 0.01) {
+    if (Math.random() < 0.1) {
       computer.dy = -computer.dy;
     }
     computer.y += computer.dy;
