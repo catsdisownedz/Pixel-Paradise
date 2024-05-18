a0ea2d1eb08dd1dc73ec05135ef8aa0c5d53a846
zeina <catsdisownedz@gmail.com>
2024-05-18 03:34:09 +0300

    added php
    made placeholder php till the table is made in the database
    -should replace the username1,username2,username3
    
    yay

--- DatabaseConnection.php
+++ DatabaseConnection.php
@@ -1,11 +1,34 @@
 <?php
-$host="trivia-game2.clsy2siwoygw.eu-north-1.rds.amazonaws.com";
-$username="admin";
-$password="yahood123";
-$database="PixelParadise";
-$conn=mysqli_connect($host,$username,$password,$database);
 
-if(!$conn){
-    die("Connection Failed". mysqli_connect_error());
+$host = "trivia-game2.clsy2siwoygw.eu-north-1.rds.amazonaws.com";
+$username = "admin";
+$password = "yahood123";
+$database = "PixelParadise";
+
+$conn = mysqli_connect($host, $username, $password, $database);
+
+if (!$conn) {
+  die("Connection Failed: " . mysqli_connect_error());
 }
 
+
+function getTopScorers() {
+    global $conn; 
+  
+    $sql = "SELECT username, SUM(game1 + game2 + game3 + game4) AS total_score
+            FROM GameScores
+            GROUP BY username
+            ORDER BY total_score DESC
+            LIMIT 3;"; // limit to first 3
+    $stmt = mysqli_prepare($conn, $sql);
+    $stmt->execute();
+    $leaderboardData = [];
+    while ($row = mysqli_fetch_assoc($stmt)) {
+      $leaderboardData[] = $row;
+    }
+    mysqli_stmt_close($stmt); 
+    return $leaderboardData;
+  }
+  
+
+?>
